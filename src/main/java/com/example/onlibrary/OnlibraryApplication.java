8f44d34b9d0cb27452155179f38ed5bdb50ecd2c
tkc1009 <tkc1009@users.noreply.github.com>
2020-05-28 23:59:32 +0900

    Spring Initializr

---
+++ src/main/java/com/example/onlibrary/OnlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlibrary/OnlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
